8303918f66a710df52b8d4a70723f8bda7da7c03
beotrix <beotrix3@mail.ru>
2021-12-01 02:57:38 +0300

    react-mesto-api-full

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/order */
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -20,16 +19,32 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const options = {
-  origin: '*',
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-  credentials: true,
-};
+const allowedCors = [
+  'https://mesto.frontend.beotrix.nomoredomains.rocks',
+  'http://mesto.frontend.beotrix.nomoredomains.rocks',
+  'https://api.mesto.beotrix.nomoredomains.rocks',
+  'localhost:3000',
+];
 
-app.use('*', cors(options));
+// eslint-disable-next-line consistent-return
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.status(200).send();
+  }
+
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
